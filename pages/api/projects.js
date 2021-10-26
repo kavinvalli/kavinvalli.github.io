00cbdf6d7ab44e2c28a1ea46dde801dd978295ee
Kavin Valli <kavinvalli@gmail.com>
2021-10-26 23:57:41 +0530

    feat: Add CORS support

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,4 +1,13 @@
-export default function handler(req, res) {
+import Cors from "cors";
+import runMiddleware from "../../utils/runMiddleware";
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
   if (req.method === "GET") {
     const projects = [
       {
---
+++ utils/runMiddleware.js
@@ -0,0 +1,11 @@
+export default function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
