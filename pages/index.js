b1cd9e6c9a07b252933654f363b2d736b1eae9f7
Kavin Valli <kavinvalli@gmail.com>
2021-10-27 10:21:44 +0530

    feat: Add n.kavin.me link

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,12 @@ export default function Home() {
       <h1>
         kavinvalli:$ <span className={styles.help}>type help to start</span>
       </h1>
+      <p>
+        Visit{" "}
+        <a href="https://n.kavin.me" target="_blank" rel="noreferrer">
+          Normal website
+        </a>
+      </p>
 
       <Terminal />
     </div>
