7d54adde2f3f01217211c2bd81a41154b3b574c5
Kavin Valli <mail@kavin.me>
2022-02-24 21:54:17 +0530

    chore: add age to about

--- utils/commandHelper.js
+++ utils/commandHelper.js
@@ -70,7 +70,9 @@ export const CONTENTS = {
     ).join("") +
     `<br />
       <div class="command">Type one of the above to view. For eg. <span style="color: var(--secondary)">about</span></div>`,
-  about: () => `My name is Kavin and I\'m a fullstack web developer
+  about: () => `My name is Kavin. I am ${getAge(
+    "December 25, 2005"
+  )} and I\'m a fullstack web developer
     <br/><br/>
     I love coding in Javascript, Typescript and Python, and have worked with frameworks like ReactJS, VueJS, Express, and Django.
     <br /><br />
@@ -100,3 +102,14 @@ export const CONTENTS = {
     return "";
   },
 };
+
+function getAge(dateString) {
+  const today = new Date();
+  const birthDate = new Date(dateString);
+
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) age--;
+
+  return age;
+}
