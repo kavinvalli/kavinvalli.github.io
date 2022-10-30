7ab772b6a6d99bc84680b913cae156f69e0d449b
Kavin Valli <mail@kavin.me>
2022-10-30 21:48:10 +0530

    chore: change projects - remove too many unnecessary ones

--- pages/api/projects.js
+++ pages/api/projects.js
@@ -53,14 +53,6 @@ export default async function handler(req, res) {
         image: "sudocrypt.png",
       },
       {
-        name: "Exun Notifications Platform (Cannot share code/link)",
-        description:
-          "Notification management and realtime notification platform built with NodeJS and Express. Used to send push notifications to the Exun 2020 App during the 25th International Tech Symposium, Exun 2020",
-        stack: ["Javascript", "NodeJS", "Express"],
-        link: "https://exunclan.com",
-        image: "exun.png",
-      },
-      {
         name: "CBSE Rebrand",
         description:
           "Rebranded CBSE Platform made using AdonisJS for CORE 2021. Creative Winners",
@@ -77,75 +69,12 @@ export default async function handler(req, res) {
         image: "task-bot.png",
       },
       {
-        name: "Job Application Portal",
-        description:
-          "[e-Lite 2020] Complete solution for applying to new and managing existing jobs of the KoolKidsKlub (fluff)",
-        stack: ["Python", "Django"],
-        link: "https://github.com/kavin25/koolkidsexun",
-        image: "kke.jpeg",
-      },
-      {
-        name: "Trivy",
-        description:
-          "Airbnb for Tour Guides. Explore the city with people who know it the best. Finalist in XINO's Exquisite hackathon.",
-        stack: ["Python", "Django"],
-        link: "https://github.com/kavin25/Xino-Hackathon2020",
-        image: "trivy.png",
-      },
-      {
-        name: "Ambulify Landing Page",
-        description:
-          'The landing page for "Ambulify", a product created at Innovate@trix 2021 competition',
-        stack: ["Javascript", "NextJS"],
-        link: "https://github.com/kavin25/innovate-trix-2021-static",
-        image: "ambulify.png",
-      },
-      {
-        name: "DPS Notices Tracker Discord Bot",
-        description:
-          "A Discord bot to track my school's notices and send a discord msg when it's been updated",
-        stack: ["Python"],
-        link: "https://github.com/kavin25/dps-notices-discord",
-        image: "dps.jpeg",
-      },
-      {
-        name: "DPS Notices Tracker Email Bot",
-        description:
-          "A bot to track my school's notices and send an email when it's been updated",
-        stack: ["Python"],
-        link: "https://github.com/kavin25/dps-notices-discord",
-        image: "dps.jpeg",
-      },
-      {
-        name: "Snapix",
-        description:
-          "A landing page (and discussion section), for a Video Editing Software. Winner of Script@trix 2020",
-        stack: ["Python", "Django"],
-        link: "https://github.com/kavin25/scriptatrix20",
-        image: "snapix.png",
-      },
-      {
         name: "Cricket VSCode Extension",
         description:
           "A VSCode Extension to show Cricket News and LiveScores from inside the editor",
         stack: ["Javascript"],
         link: "https://github.com/kavin25/cricket-vscode-extension",
         image: "cricket-vscode.png",
-      },
-      {
-        name: "URL Shortener",
-        description:
-          "A URL Shortener written in NodeJS and Express with MongoDB",
-        stack: ["Javascript", "NodeJS", "Express"],
-        link: "https://github.com/kavin25/url-shortener",
-        image: "url.png",
-      },
-      {
-        name: "FourEss Games",
-        description: "A screenshare based quiz app for friends and family",
-        stack: ["Python", "Django"],
-        link: "https://github.com/kavin25/jeopardyquiz-with-django",
-        image: "quiz.png",
       },
     ];
 
