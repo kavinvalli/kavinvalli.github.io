2dc19efb1fa6afb5ef608033f97c318a4cd3858b
Kavin Valli <mail@kavin.me>
2025-08-10 18:09:47 -0700

    fix: move [shortlink] to s/[shortlink] as a hack workaround around ddos

--- app/s/[shortlink]/route.js
+++ app/s/[shortlink]/route.js
@@ -3,21 +3,16 @@ import { redirect } from "next/navigation";
 export async function GET(request, { params }) {
   const { shortlink } = params;
 
-  console.log({ shortlink });
-
   if (!shortlink) {
     return new Response("No shortlink provided", { status: 400 });
   }
 
   const url = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${process.env.AIRTABLE_TABLE_ID}?filterByFormula=slug = "${shortlink}"`;
-  console.log({ url });
   const res = await fetch(encodeURI(url), {
     headers: {
       Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}`,
     },
   });
-
-  console.log({ res });
 
   if (!res.ok) {
     return new Response("Error fetching data", { status: 500 });
