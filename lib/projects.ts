97dd6d4145f38194ad65b76d1de3e4e7a78d6fbe
Kavin Valli <mail@kavin.me>
2026-08-14 23:10:58 -0400

    time to scrap some projects

--- lib/projects.ts
+++ lib/projects.ts
@@ -31,27 +31,27 @@ export const projects: Project[] = [
     stack: ["TypeScript"],
     link: "https://typewind.vercel.app",
   },
-  {
-    name: "Wunderkind — DPS Goethe Quiz",
-    description:
-      "A quiz platform for Goethe Institut × DPS Society, used by 9,000+ students across two iterations with a 99% availability rating.",
-    stack: ["PHP", "Laravel", "TypeScript", "React"],
-    link: "https://dpsgoethequiz.com",
-  },
-  {
-    name: "Sudocrypt v11.0",
-    description:
-      "The platform for Exun Clan's cryptic hunt — 1.5k+ participants and 50k+ attempts over two days.",
-    stack: ["PHP", "Laravel", "TypeScript", "React"],
-    link: "https://github.com/kavinvalli/sudocrypt-v11",
-  },
-  {
-    name: "Cognizer",
-    description:
-      "A Chrome extension to connect with people during and after conferences. 2nd runner-up at Code Warriors soBig Hackathon 2021.",
-    stack: ["JavaScript", "Node.js", "Chrome Extension API"],
-    link: "https://cognizer.kavin.me/",
-  },
+  // {
+  //   name: "Wunderkind — DPS Goethe Quiz",
+  //   description:
+  //     "A quiz platform for Goethe Institut × DPS Society, used by 9,000+ students across two iterations with a 99% availability rating.",
+  //   stack: ["PHP", "Laravel", "TypeScript", "React"],
+  //   link: "https://dpsgoethequiz.com",
+  // },
+  // {
+  //   name: "Sudocrypt v11.0",
+  //   description:
+  //     "The platform for Exun Clan's cryptic hunt — 1.5k+ participants and 50k+ attempts over two days.",
+  //   stack: ["PHP", "Laravel", "TypeScript", "React"],
+  //   link: "https://github.com/kavinvalli/sudocrypt-v11",
+  // },
+  // {
+  //   name: "Cognizer",
+  //   description:
+  //     "A Chrome extension to connect with people during and after conferences. 2nd runner-up at Code Warriors soBig Hackathon 2021.",
+  //   stack: ["JavaScript", "Node.js", "Chrome Extension API"],
+  //   link: "https://cognizer.kavin.me/",
+  // },
   {
     name: "Cricket VSCode Extension",
     description:
